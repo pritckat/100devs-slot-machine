693e80c71276e88f56e4d26ae931c30a4b15f1ad
pritckat <pritckat@gmail.com>
2021-02-07 18:45:27 -0500

    add win results

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ class SlotMachine {
         this.reel2 = ""
         this.reel3 = ""
         this.coins = 100
+        this.bet = 0
+    }
+
+    get reels() {
+        return [this.reel1, this.reel2, this.reel3]
     }
 
     static get reelOptions() {
@@ -18,10 +23,39 @@ class SlotMachine {
     }
 
     placeBet(amount) {
+        this.bet = amount
         this.coins -= amount
     }
+
+    getResult() {
+        if (this.reels.includes("bomb")) {return}
+        else if (this.reels.every(e => e == "seven")) {
+            this.coins += this.bet * 10
+        }
+        else if (this.reel3 == this.reel1 && this.reel1 == this.reel2) {
+            this.coins += 3 * this.bet
+        }
+        else if (this.reels.includes("seven")) {
+            this.coins += this.bet * this.reels.filter(e => e == "seven").length
+        }
+    }
+
+    play() {
+        
+    }
+    
 }
 
 let sm = new SlotMachine
-sm.pullLever()
-console.log(sm.reel1, sm.reel2, sm.reel3)
+console.log(sm)
+sm.bet = 10
+sm.reel3 = 'seven'
+console.log(sm)
+sm.getResult()
+console.log(sm)
+sm.reel2 = 'seven'
+sm.getResult()
+console.log(sm)
+sm.reel1 = 'bomb'
+sm.getResult()
+console.log(sm)
