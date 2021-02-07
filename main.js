e408632327889705342d6cf2686d1af0c99d9a56
pritckat <pritckat@gmail.com>
2021-02-07 12:45:33 -0500

    add slot machine options and pullable lever

---
+++ main.js
@@ -0,0 +1,22 @@
+class SlotMachine {
+    constructor() {
+        this.reel1 = ""
+        this.reel2 = ""
+        this.reel3 = ""
+    }
+
+    static get reelOptions() {
+        return ['cherry', 'bomb', 'seven', 'star', 'coin']
+    }
+
+    pullLever() {
+        let options = SlotMachine.reelOptions
+        this.reel1 = options[Math.floor(Math.random() * 5)]
+        this.reel2 = options[Math.floor(Math.random() * 5)]
+        this.reel3 = options[Math.floor(Math.random() * 5)]
+    }
+}
+
+let sm = new SlotMachine
+sm.pullLever()
+console.log(sm.reel1, sm.reel2, sm.reel3)
