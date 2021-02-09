de5d04c66d5606a8199579ba5c3f5ff30809235d
pritckat <pritckat@gmail.com>
2021-02-09 16:53:27 -0500

    update reel option names to match images

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ class SlotMachine {
         this.reel1 = ""
         this.reel2 = ""
         this.reel3 = ""
-        this.coins = 100
+        this.diamonds = 100
         this.bet = 0
     }
 
@@ -12,7 +12,7 @@ class SlotMachine {
     }
 
     static get reelOptions() {
-        return ['cherry', 'bomb', 'seven', 'star', 'coin']
+        return ['cherry', 'lemon', 'seven', 'bar', 'diamond']
     }
 
     pullLever() {
@@ -25,29 +25,27 @@ class SlotMachine {
     placeBet(amount) {
         amount = Math.floor(amount)
         if (amount == 0) {return false}
-        if (amount > this.coins) {amount = this.coins}
+        if (amount > this.diamonds) {amount = this.diamonds}
         this.bet = amount
-        this.coins -= amount
+        this.diamonds -= amount
         console.log(this.bet)
         return true
     }
 
     getResult() {
-        if (this.reels.includes("bomb")) {
-            console.log('bomb')
+        if (this.reels.includes("lemon")) {
             return}
         else if (this.reels.every(e => e == "seven")) {
-            console.log('all sevens!')
-            this.coins += this.bet * 10
+            this.diamonds += this.bet * 10
         }
         else if (this.reel3 == this.reel1 && this.reel1 == this.reel2) {
-            this.coins += 3 * this.bet
+            this.diamonds += 3 * this.bet
         }
         else if (this.reels.includes("seven")) {
-            this.coins += this.bet * this.reels.filter(e => e == "seven").length
+            this.diamonds += this.bet * this.reels.filter(e => e == "seven").length
         }
         else if ((this.reel1 == this.reel2) || this.reel1 == this.reel3 || this.reel2 == this.reel3) {
-            this.coins += Math.floor(this.bet *.5)
+            this.diamonds += Math.floor(this.bet *.5)
         }
     }
 
@@ -59,7 +57,7 @@ class SlotMachine {
         console.log(this)
         this.getResult()
         console.log(this)
-        if (this.coins == 0) {alert('you lose')}
+        if (this.diamonds == 0) {alert('you lose')}
     }
     
 }
