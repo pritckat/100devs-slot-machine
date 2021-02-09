6dbc1964f7c7524c495471a7ca0573178c0ff36d
pritckat <pritckat@gmail.com>
2021-02-09 18:04:32 -0500

    pictures update with reels

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ class SlotMachine {
         this.reel1 = ""
         this.reel2 = ""
         this.reel3 = ""
-        this.diamonds = 100
+        this.coins = 100
         this.bet = 0
     }
 
@@ -25,9 +25,9 @@ class SlotMachine {
     placeBet(amount) {
         amount = Math.floor(amount)
         if (amount == 0) {return false}
-        if (amount > this.diamonds) {amount = this.diamonds}
+        if (amount > this.coins) {amount = this.coins}
         this.bet = amount
-        this.diamonds -= amount
+        this.coins -= amount
         console.log(this.bet)
         return true
     }
@@ -36,31 +36,46 @@ class SlotMachine {
         if (this.reels.includes("lemon")) {
             return}
         else if (this.reels.every(e => e == "seven")) {
-            this.diamonds += this.bet * 10
+            this.coins += this.bet * 10
         }
         else if (this.reel3 == this.reel1 && this.reel1 == this.reel2) {
-            this.diamonds += 3 * this.bet
+            this.coins += 3 * this.bet
         }
         else if (this.reels.includes("seven")) {
-            this.diamonds += this.bet * this.reels.filter(e => e == "seven").length
+            this.coins += this.bet * this.reels.filter(e => e == "seven").length
         }
         else if ((this.reel1 == this.reel2) || this.reel1 == this.reel3 || this.reel2 == this.reel3) {
-            this.diamonds += Math.floor(this.bet *.5)
+            this.coins += Math.floor(this.bet *.5)
         }
     }
 
     play(amount) {
-        if (!this.placeBet(amount)) {
-            console.log (this.placeBet(amount))
-            return}
+        if (!this.placeBet(amount)) {return}
         this.pullLever()
-        console.log(this)
         this.getResult()
-        console.log(this)
         if (this.diamonds == 0) {alert('you lose')}
     }
-    
 }
 
+function assignPic(reel) {
+    switch (reel) {
+        case 'cherry':
+            return 'images/cherry.png'
+        case 'bar':
+            return 'images/bar.png'
+        case 'diamond':
+            return 'images/diamond.png'
+        case 'lemon':
+            return 'images/lemon.png'
+        case 'seven':
+            return 'images/seven.png'
+    }
+}
 let sm = new SlotMachine
 sm.play(10)
+const reel1 = document.querySelector("#reel1")
+const reel2 = document.querySelector("#reel2")
+const reel3 = document.querySelector("#reel3")
+reel1.src = assignPic(sm.reel1)
+reel2.src = assignPic(sm.reel2)
+reel3.src = assignPic(sm.reel3)
